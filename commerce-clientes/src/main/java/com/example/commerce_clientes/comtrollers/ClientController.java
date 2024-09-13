98ab86777540326d935361f48890cda570591c8c
Erick Lemos <ericklemos.dev@gmail.com>
2024-09-13 18:39:17 -0300

    Metodo getOne client

--- commerce-clientes/src/main/java/com/example/commerce_clientes/comtrollers/ClientController.java
+++ commerce-clientes/src/main/java/com/example/commerce_clientes/comtrollers/ClientController.java
@@ -9,12 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ClientController {
@@ -35,7 +35,16 @@ public class ClientController {
         return ResponseEntity.status(HttpStatus.OK).body(clientList);
     }
 
-    
+    @GetMapping("/clients/{idClient}")
+    public ResponseEntity<Object> getOne(@PathVariable("idClient") UUID idClient){
+        Optional<ClientModel> clientOpt = clientRepository.findById(idClient);
+        if(clientOpt.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not Found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(clientOpt.get());
+    }
+
+
 
 
 }
