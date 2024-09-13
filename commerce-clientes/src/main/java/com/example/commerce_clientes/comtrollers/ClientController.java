bd03b9aa86bad30392ea6d849d002ddc9615f9c6
Erick Lemos <ericklemos.dev@gmail.com>
2024-09-13 18:33:38 -0300

    metodo getAll clients

--- commerce-clientes/src/main/java/com/example/commerce_clientes/comtrollers/ClientController.java
+++ commerce-clientes/src/main/java/com/example/commerce_clientes/comtrollers/ClientController.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ClientController {
@@ -23,8 +26,16 @@ public class ClientController {
     public ResponseEntity<ClientModel> saveClient(@RequestBody @Valid ClientRecordDto clientRecordDto){
         ClientModel clientModel = new ClientModel();
         BeanUtils.copyProperties(clientRecordDto, clientModel);
-        return ResponseEntity.status(HttpStatus.OK).body(clientRepository.save(clientModel));
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientRepository.save(clientModel));
     }
+
+    @GetMapping("/clients")
+    public ResponseEntity<List<ClientModel>> getAll(){
+        List<ClientModel> clientList = clientRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(clientList);
+    }
+
+    
 
 
 }
